b50187d294989c94d766ab30b2f325ed6080bf02
Drummer7777777 <73654451+Drummer7777777@users.noreply.github.com>
2021-03-10 22:50:59 +0500

    Delete insert_in_htmll.py

--- insert_in_htmll.py
+++
@@ -1,5 +0,0 @@
-def insert_data(nikname,naz,vih_type,level,tank,count_battle,percent_win,rank,mark):
-
-    line = f'<tr><td>{naz}</td><td>{vih_type}</td><td>{level}</td><td>{tank}</td><td>{count_battle}</td><td>{percent_win}</td><td>{rank}</td><td>{mark}</td></tr>\n'
-    with open(f'{nikname}.txt', 'a+') as fl:#, encoding='utf-8') as fl:
-        fl.writelines(line)
